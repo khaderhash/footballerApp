5f6cf15e593d6e66ab9529946d5e09efdef626a1
khader <khadergamer03@gmail.com>
2025-08-17 20:42:57 +0300

    edit splash view and app splashviewbody widget to this file

--- lib/features/splash/presntation/view/splash_view.dart
+++ lib/features/splash/presntation/view/splash_view.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatefulWidget {
+import 'Widgets/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
-  State<SplashView> createState() => _SplashViewState();
-}
-
-class _SplashViewState extends State<SplashView> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SplashViewBody();
   }
 }
