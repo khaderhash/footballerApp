560c87b28b0ed8205f4b58b69d7a823cb0e6a196
khader <khadergamer03@gmail.com>
2025-08-19 17:08:54 +0300

    splash view ui done

--- lib/features/splash/presntation/views/Widgets/splash_view_body.dart
+++ lib/features/splash/presntation/views/Widgets/splash_view_body.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utlils/Assets.dart';
+
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [Image.asset(AssetsImage.logo)],
     );
   }
 }
